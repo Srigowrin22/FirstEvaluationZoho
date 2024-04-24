7941d87bf3009778e3e43a1ba84fcaf5404896f1
Srigowri N <126903290+Srigowrin22@users.noreply.github.com>
2024-04-24 18:54:05 +0530

    Update Problem3.java

--- Problem3.java
+++ Problem3.java
@@ -7,18 +7,42 @@ public class Problem3 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		String a = "";
-		String b = "";
+		int a[] = new int[n];
+		int b[] = new int [m];
+		int max = Math.max(n,m);
+		
+		int res[] = new int[max+1];
 		for(int i=0; i<n; i++) {
-			a+=sc.next();
+			a[i]=sc.nextInt();
 		}
 		for(int i=0; i<m; i++) {
-			b+=sc.next();
+			b[i]=sc.nextInt();
+		}
+		int carry =0, k=max-1;
+		int x=n-1, y=m-1;
+		
+		while(x>=0 && y>=0) {
+			int s = a[x]+b[y]+carry;
+			res[k]=s%10;
+			carry = s/10;
+			x--; y--; k--;			
+		}
+		while(x>=0) {
+			int s = a[x]+carry;
+			res[k]=s%10;
+			carry = s/10;
+			x--; y--; k--;	
 		}
 		
-			
-		int c[] = new int[n+m];
-		int min = Math.min(n,m);
+		while(y>=0) {
+			int s = b[y]+carry;
+			res[k]=s%10;
+			carry = s/10;
+			x--; y--; k--;	
+		}
 		
+		for(int i: res) {
+			System.out.print(i+" ");
+		}
 	}
 }
